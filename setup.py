d327b03a0f03a17543a24c22adb30ed87b889b8f
Kevin McCormack <harlemsquirrel@mccormack.tech>
2021-01-26 22:43:44 -0500

    Remove Python 3.4 support

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.4',
+    python_requires='>=3.5',
     keywords=['vivotek', 'Camera', 'IPC'],
     test_suite="tests",
 )
