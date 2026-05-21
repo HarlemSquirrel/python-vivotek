a7b5b8e42befed1591b22fd1bce9337fde94da89
Kevin McCormack <kevin@mccormack.tech>
2026-05-20 21:37:29 -0400

    Change async device info setting to synchronous
    Replaced asynchronous device info setting with synchronous call.

--- sample.py
+++ sample.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-import asyncio
-
 import keyring
 
 from libpyvivotek import VivotekCamera
@@ -9,7 +7,7 @@ from libpyvivotek import VivotekCamera
 cam = VivotekCamera(host='192.168.2.184', port=80, usr='root',
                     pwd=keyring.get_password('camera', 'root'),
                     sec_lvl="admin")
-asyncio.run(cam.async_set_device_info())
+cam.set_device_info()
 print(f"Camera model:  {cam.model_name}")
 print(f"Firmware:      {cam.get_firmware_version()}")
 print(f"Network info:  {cam.get_param('status_eth_i0')}")
